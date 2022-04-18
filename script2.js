dc0d37f458b42c9129487a035e83c0eabbca5c03
Daira <dairafaz@gmail.com>
2022-04-18 15:23:16 -0300

    testeo slider

--- script2.js
+++ script2.js
@@ -1,3 +1,4 @@
+
 function commentMatch(className){
   let topPos = document.getElementsByClassName(className)[0].offsetTop;
   document.getElementsByClassName(className)[1].style.top = topPos + "px";
